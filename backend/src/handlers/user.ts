ca3a647768dbb9ccf4edd14fd2611b63ce0c2012
firdaus <firdausthedev@gmail.com>
2023-12-07 16:08:50 +0800

    crud user

--- backend/src/handlers/user.ts
+++ backend/src/handlers/user.ts
@@ -1,6 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 import prisma from "../utils/connectDb";
-import { hashPassword, createJWT, comparePasswords } from "../utils/auth";
+import {
+  hashPassword,
+  createJWT,
+  comparePasswords,
+  AuthenticatedRequest,
+} from "../utils/auth";
 
 export const createUser = async (
   req: Request,
@@ -55,6 +60,116 @@ export const signin = async (
     }
     const token = createJWT(user!, "user");
     res.json({ token, success: true });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAllUsers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const users = await prisma.user.findMany({
+      select: {
+        username: true,
+        id: true,
+        bookings: true,
+        Review: true,
+      },
+    });
+
+    res.json({ message: users, success: true });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateUser = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    if (req.body.new_password) {
+      if (!req.body.old_password) {
+        res.status(400);
+        res.json({
+          message: "Please provide old password",
+          success: false,
+        });
+        return;
+      }
+
+      const user = await prisma.user.findUnique({
+        where: { id: req.user!.id },
+      });
+
+      if (!user) {
+        res.status(404);
+        res.json({
+          message: "User does not exist",
+          success: false,
+        });
+        return;
+      }
+
+      const isValid = await comparePasswords(
+        req.body.old_password,
+        user.password,
+      );
+
+      if (!isValid) {
+        res.status(401);
+        res.json({
+          message: "Password and username does not match",
+          success: false,
+        });
+        return;
+      }
+
+      const hashedPassword = await hashPassword(req.body.new_password);
+      const updatedUser = await prisma.user.update({
+        where: { id: req.user!.id },
+        data: {
+          username: req.body.username,
+          password: hashedPassword,
+        },
+        select: {
+          username: true,
+        },
+      });
+      res.json({ message: updatedUser, success: true });
+    } else {
+      const updatedUser = await prisma.user.update({
+        where: { id: req.user!.id },
+        data: {
+          username: req.body.username,
+        },
+      });
+
+      res.json({ message: updatedUser, success: true });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const deletedUser = await prisma.user.delete({
+      where: { id: req.user!.id },
+      select: {
+        username: true,
+      },
+    });
+
+    res.json({ message: deletedUser, success: true });
   } catch (error) {
     next(error);
   }
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,7 +1,14 @@
 import { Router } from "express";
 import { handleInputErrors } from "../middlewares/handleInputsErrors";
-import { body } from "express-validator";
-import { createUser, signin } from "../handlers/user";
+import { body, checkExact } from "express-validator";
+import {
+  createUser,
+  deleteUser,
+  getAllUsers,
+  signin,
+  updateUser,
+} from "../handlers/user";
+import { protectedAdmin, protectedUser } from "../utils/auth";
 
 const router = Router();
 
@@ -39,5 +46,47 @@ router.post(
   handleInputErrors,
   signin,
 );
+
+/**
+ * @feature User update
+ * @route   PUT /api/user
+ * @access  Private
+ */
+router.put(
+  "/",
+  protectedUser,
+  checkExact([
+    body("username")
+      .optional()
+      .isString()
+      .isLength({ min: 4, max: 32 })
+      .withMessage("Username must be between 4 and 32 characters"),
+    body("old_password").optional().isString(),
+    body("new_password")
+      .optional()
+      .isString()
+      .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&_]{6,32}$/)
+      .withMessage(
+        `Please enter a password at least 6 character and contain At least one uppercase. At least one lower case.
+       At least one number. At least one special character.`,
+      ),
+  ]),
+  handleInputErrors,
+  updateUser,
+);
+
+/**
+ * @feature User delete
+ * @route   DELETE /api/user
+ * @access  Private
+ */
+router.delete("/", protectedUser, deleteUser);
+
+/**
+ * @feature admin get all users
+ * @route   GET /api/user/admin
+ * @access  Private
+ */
+router.get("/admin", protectedAdmin, getAllUsers);
 
 export default router;
