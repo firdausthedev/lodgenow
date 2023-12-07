84b6a74240768c0654045b9286b52288a23ebde3
firdaus <firdausthedev@gmail.com>
2023-12-07 18:53:45 +0800

    add error code in error middleware

--- backend/src/middlewares/errors.ts
+++ backend/src/middlewares/errors.ts
@@ -19,12 +19,17 @@ export const errorHandler = (
       return res
         .json({
           message: `There is a unique constraint violation on ${err.meta?.target}`,
+          errCode: err.code,
           success: false,
         })
         .status(401);
     } else {
-      return res.json({ message: err.meta, success: false }).status(401);
+      return res
+        .json({ message: err.meta, errCode: err.code, success: false })
+        .status(401);
     }
+  } else if (err instanceof Prisma.PrismaClientValidationError) {
+    return res.json({ message: "Unknown field", success: false }).status(401);
   } else {
     // uncaught errors
     return res.json({ message: "server error", success: false }).status(500);
