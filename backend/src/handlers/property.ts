6a8bae5dda5e267e39bd3e545257e9ba4415af2e
firdaus <firdausthedev@gmail.com>
2023-12-07 18:55:56 +0800

    crud property

--- backend/src/handlers/property.ts
+++ backend/src/handlers/property.ts
@@ -20,9 +20,7 @@ export const getAllProperty = async (
           .json({ message: "Invalid type", success: false });
       }
       const propertiesByType = await prisma.property.findMany({
-        where: {
-          type: type,
-        },
+        where: { type: type },
         orderBy: {
           name: "asc",
         },
@@ -97,6 +95,76 @@ export const geOneProperty = async (
       },
     });
     res.json({ data: property, success: true });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createProperty = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const property = await prisma.property.create({
+      data: req.body,
+    });
+    res.json({ data: property, success: true });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateProperty = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const updatedProperty = await prisma.property.update({
+      where: { id: req.params.id },
+      data: req.body,
+    });
+    res.json({ data: updatedProperty, success: true });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteProperty = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const deletedProperty = await prisma.property.delete({
+      where: { id: req.params.id },
+    });
+    res.json({ data: deletedProperty, success: true });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAllPropertyAdmin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const properties = await prisma.property.findMany({
+      include: {
+        agent: true,
+        reviews: true,
+        bookings: {
+          include: {
+            user: { select: { username: true, id: true } },
+            payment: true,
+          },
+        },
+      },
+    });
+    res.json({ data: properties, success: true });
   } catch (error) {
     next(error);
   }
--- backend/src/routes/property.ts
+++ backend/src/routes/property.ts
@@ -1,22 +1,85 @@
 import { Router } from "express";
-// import { handleInputErrors } from "../middlewares/handleInputsErrors";
-import { getAllProperty, geOneProperty } from "../handlers/property";
-// import { body } from "express-validator";
-
+import {
+  getAllProperty,
+  geOneProperty,
+  createProperty,
+  updateProperty,
+  deleteProperty,
+  getAllPropertyAdmin,
+} from "../handlers/property";
+import { protectedAdmin } from "../utils/auth";
+import { body, checkExact } from "express-validator";
+import { handleInputErrors } from "../middlewares/handleInputsErrors";
 const router = Router();
 
 /**
  * @feature Get all properties
- * @route   GET /api/property or /api/properties?type=
+ * @route   GET /api/property or /api/property?type=
  * @access  Public
  */
 router.get("/", getAllProperty);
 
 /**
+ * @feature admin get all property
+ * @route   GET /api/property/admin
+ * @access  Private
+ */
+router.get("/admin", protectedAdmin, getAllPropertyAdmin);
+
+/**
  * @feature Get single properties
- * @route   GET /api/properties/:id
+ * @route   GET /api/property/:id
  * @access  Public
  */
 router.get("/:id", geOneProperty);
+
+/**
+ * @feature admin create property
+ * @route   POST /api/property/admin
+ * @access  Private
+ */
+router.post(
+  "/admin",
+  protectedAdmin,
+  body("name").isString().isLength({ min: 5, max: 255 }),
+  body("location").isString().isLength({ min: 5, max: 255 }),
+  body("price").isFloat({ min: 1 }),
+  body("bedrooms").isInt({ min: 0 }),
+  body("bathrooms").isInt({ min: 0 }),
+  body("photos").isArray({ min: 1, max: 5 }),
+  body("type").isIn(["CITY", "RURAL", "MOUNTAIN", "TROPICAL"]),
+  body("agentId").isString(),
+  handleInputErrors,
+  createProperty,
+);
+
+/**
+ * @feature admin update property
+ * @route   PUT /api/property/admin/:id
+ * @access  Private
+ */
+router.put(
+  "/admin/:id",
+  protectedAdmin,
+  checkExact([
+    body("name").isString().isLength({ min: 5, max: 255 }),
+    body("location").isString().isLength({ min: 5, max: 255 }),
+    body("price").isFloat({ min: 1 }),
+    body("bedrooms").isInt({ min: 0 }),
+    body("bathrooms").isInt({ min: 0 }),
+    body("photos").isArray({ min: 1, max: 5 }),
+    body("type").isIn(["CITY", "RURAL", "MOUNTAIN", "TROPICAL"]),
+    body("agentId").isString(),
+  ]),
+  handleInputErrors,
+  updateProperty,
+);
+
+/**
+ * @feature admin delete property
+ * @route   DELETE /api/property/admin/:id
+ * @access  Private
+ */
+router.delete("/admin/:id", protectedAdmin, deleteProperty);
 
 export default router;
